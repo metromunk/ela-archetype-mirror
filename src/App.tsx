7ebeec08e6346405d5eca4cfaab7f6ef376376a7
metromunk <reachmetromunk@gmail.com>
2025-11-14 02:09:17 +1100

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="p-10 text-center">
-      <h1 className="text-3xl font-bold">ELA Archetype Mirror</h1>
-      <p className="mt-4 text-lg">App is working.</p>
-    </div>
-  );
-}
